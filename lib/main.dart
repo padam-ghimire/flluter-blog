0bb0ab2878a160e3244efd959d121f80f9878540
Padam Ghimire <padamghimire75@gmail.com>
2020-04-03 23:27:03 +0545

    Project started

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+import 'widget/app.dart';
+
+void main() => runApp(App());
+
---
+++ lib/widget/app.dart
@@ -0,0 +1,14 @@
+
+
+
+import 'package:blog/screens/home.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home()
+    );
+  }
+}
